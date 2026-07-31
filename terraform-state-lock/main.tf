340b030b253be9a14804c1b16518d0e3197d9851
moyusuf44 <mohamedyusuf.6445@gmail.com>
2026-07-31 23:32:39 +0300

    changes to actions

--- terraform-state-lock/main.tf
+++ terraform-state-lock/main.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "terraform_locks" {
+resource "aws_dynamodb_table" "terraform-codeserver-state-lock" {
   name         = "terraform-codeserver-state-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
